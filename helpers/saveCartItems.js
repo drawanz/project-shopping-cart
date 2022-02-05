308266e852edd910179a8461233e6544f4c60f97
Leonidas <leonidas.drawanz@gmail.com>
2022-02-05 08:18:09 -0300

    requisito 10

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,7 +1,11 @@
-const saveCartItems = () => {
-  const ol = document.querySelector('.cart__items');
-  const olHtml = ol.innerHTML;
-  localStorage.setItem('cartItems', olHtml);
+const saveCartItems = (item) => {
+  if (item === undefined) {
+    const ol = document.querySelector('.cart__items');
+    const olHtml = ol.innerHTML;
+    localStorage.setItem('cartItems', olHtml);
+  } else {
+    localStorage.setItem('cartItems', item);
+  }
 };
 
 if (typeof module !== 'undefined') {
